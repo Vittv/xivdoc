a2b5dff6b123f9e80e86d49e017e67b83278c7ed
Vittv <166668150+Vittv@users.noreply.github.com>
2025-04-07 17:00:02 -0300

    Redesign pt 1

--- src/shortcut/data.js
+++ src/shortcut/data.js
@@ -1,36 +1,36 @@
 const ultimateRaids = [
-    { name: "UCOB", url: "../../pages/ultimate/ucob.html" },
-    { name: "UWU", url: "../../pages/ultimate/uwu.html" },
-    { name: "TEA", url: "../../pages/ultimate/tea.html" },
-    { name: "DSR", url: "../../pages/ultimate/dsr.html" },
-    { name: "TOP", url: "../../pages/ultimate/top.html" },
-    { name: "FRU", url: "../../pages/ultimate/fru.html" }
+    { name: "UCOB", url: "/xivdoc/pages/ultimate/ucob.html" },
+    { name: "UWU", url: "/xivdoc/pages/ultimate/uwu.html" },
+    { name: "TEA", url: "/xivdoc/pages/ultimate/tea.html" },
+    { name: "DSR", url: "/xivdoc/pages/ultimate/dsr.html" },
+    { name: "TOP", url: "/xivdoc/pages/ultimate/top.html" },
+    { name: "FRU", url: "/xivdoc/pages/ultimate/fru.html" }
 ];
 
 const savageRaids = [
-    { name: "M1S", url: "../../pages/savage/m1s.html" },
-    { name: "M2S", url: "../../pages/savage/m2s.html" },
-    { name: "M3S", url: "../../pages/savage/m3s.html" },
-    { name: "M4S", url: "../../pages/savage/m4s.html" }
+    { name: "M1S", url: "/xivdoc/pages/savage/m1s.html" },
+    { name: "M2S", url: "/xivdoc/pages/savage/m2s.html" },
+    { name: "M3S", url: "/xivdoc/pages/savage/m3s.html" },
+    { name: "M4S", url: "/xivdoc/pages/savage/m4s.html" }
 ];
 
 const extremeTrials = [
-    { name: "EX1", url: "../../pages/extreme/ex1.html" },
-    { name: "EX2", url: "../../pages/extreme/ex2.html" },
-    { name: "EX3", url: "../../pages/extreme/ex3.html" }
+    { name: "EX1", url: "/xivdoc/pages/extreme/ex1.html" },
+    { name: "EX2", url: "/xivdoc/pages/extreme/ex2.html" },
+    { name: "EX3", url: "/xivdoc/pages/extreme/ex3.html" }
 ];
 
 const unrealTrials = [
-    { name: "Byakko", url: "../../pages/unreal/byakko.html" },
-    { name: "Suzaku", url: "../../pages/unreal/suzaku.html" }
+    { name: "Byakko", url: "/xivdoc/pages/unreal/byakko.html" },
+    { name: "Suzaku", url: "/xivdoc/pages/unreal/suzaku.html" }
 ];
 
 const chaoticRaids = [
-    { name: "CoD", url: "../../pages/chaotic/cod.html" }
+    { name: "CoD", url: "/xivdoc/pages/chaotic/cod.html" }
 ];
 
 const criterionDungeons = [
-    { name: "ASS", url: "../../pages/criterion/crit1.html"},
-    { name: "AMR", url: "../../pages/criterion/crit2.html"},
-    { name: "AAL", url: "../../pages/criterion/crit3.html"},
+    { name: "ASS", url: "/xivdoc/pages/criterion/crit1.html"},
+    { name: "AMR", url: "/xivdoc/pages/criterion/crit2.html"},
+    { name: "AAL", url: "/xivdoc/pages/criterion/crit3.html"},
 ];
